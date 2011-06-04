969908104ee58934d5b6f88cd60c821d393aa4bc
Justin R. LaBenne <justinlabenne@gmail.com>
2011-06-04 01:40:20 -0400

    Markup starting point

---
+++ main.master.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Compression;
+
+public partial class main : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //if (Request.Headers["Accept-encoding"] != null && Request.Headers["Accept-encoding"].Contains("gzip"))
+        //{
+        //    Response.Filter = new GZipStream(Response.Filter, CompressionMode.Compress, true);
+        //    Response.AppendHeader("Content-encoding", "gzip");
+        //}
+        //else if (Request.Headers["Accept-encoding"] != null && Request.Headers["Accept-encoding"].Contains("deflate"))
+        //{
+        //    Response.Filter = new DeflateStream(Response.Filter, CompressionMode.Compress, true);
+        //    Response.AppendHeader("Content-encoding", "deflate");
+        //}
+    }
+}
